ae98b513123edc815ffd28bceb17c5ba1740c658
jassq <1252348575@qq.com>
2017-07-02 23:17:10 +0800

    djg

--- initialization.py
+++ initialization.py
@@ -10,8 +10,11 @@ con = sqlite3.connect('generalAI.db')
 con.execute("create table if not exists emotion(parameter_name, value)")
 # con.execute("delete from emotion").rowcount ##delete the old data
 emotion=[
-    ('a',random.randint(0,100)),
-    ('b',random.randint(0,100))
+    ('select_rate',random.random()*100),
+    ('pleasure_value',0),
+    ('strength', random.random()*100),
+    ('stable', random.random()*100),
+    ('change_fun', random.randint(0,5))
 ]
 
 con.executemany("insert into emotion(parameter_name, value) values (?, ?)", emotion)
@@ -19,8 +22,8 @@ con.commit()
 para=dict(con.execute("select * from emotion"))
 print(para)
 
-for row in con.execute("select parameter_name, value from emotion"):
-    print(row)
+# for row in con.execute("select parameter_name, value from emotion"):
+#     print(row)
 
 
 
--- motivation/emotion.py
+++ motivation/emotion.py
@@ -2,19 +2,62 @@
 This file provide model for emothion.
 """
 import sqlite3
+import changefun
+
 con = sqlite3.connect('../generalAI.db')
 para=dict(con.execute("select * from emotion"))
-print(para.a)
+print(para)
 
 class Emotion():
-    """Emotion base class"""
+    """
+    Emotion base class
+    There is two side that make emotion up and down.
+    one side is directly sensor input, another side is 
+    base on comprehensive of the sensor input, like sound, 
+    scene.
+    """
     def __init__(self):
-        self.select_rate  = para['select_rate']
-        self.init_value   = para['init_value']
-        self.curent_vuale = para['current_value']
-        self.stable       = para['stable']
-        self.change_fun   = para['change_fun']
-        
+        self.select_rate  = para['select_rate']        #the rate this emotion be selected
+        self.pleasure_value   = para['pleasure_value'] #how much pleasure get from this emotion
+        self.strength = para['current_value']          #the emothon strength
+        self.stable       = para['stable']             #is this emotion easy to down the strength
+        self.change_fun   = para['change_fun'] 
         pass
+
+    def temperature(self, temperature):
+        """
+        Recieve temperature from outside, sensor,
+        change the pleasure_value and strength.
+        the function shoule result below figure:
+
+            * * * * * *
+          *             *
+         *               *
+        *                *
+        horizontal axis is temperature, vertical axis
+        is strength or pleasure_value
+        """
+        pleasure=0
+        if temperature<=-50:
+            #y=-(x+50)^2
+            pleasure = -(temperature+50)**2
+
+        if -50<temperature<-40:
+            pleasure = 0.1*temperature+5
+
+        if 40<temperature<50:
+            pleasure = -0.1*temperature+5
+
+        if -40<=temperature<=40:
+            pleasure=1
+
+        if temperature>=50:
+            pleasure = -(temperature-50)**2
+
+        return pleasure
+        pass
+
+
+    
 
 
