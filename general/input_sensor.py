83baf14fd88fe1c99e5d5f988e2b987190c132bf
Jass Ada <Jass Ada>
2017-07-14 11:11:09 +0800

    jdfj

--- general/input_sensor.py
+++ general/input_sensor.py
@@ -6,13 +6,13 @@ import numpy as np
 #temperature C
 temperature=20
 
-#eyes, every element is an image, so it is a image series
+#eyes, every element is an image, so it is a image series, take 5 samples each time.
 eyes=[[0],[0],[0],[0],[0]]
 
-#energy
+#energy, or battery
 energy=20
 
-#force
+#force, there are many force sensor on robot's body, the help to bahave and judge environment.
 ALL_FORCE_SENSOR = 10000
 force_unit={'direction':[0,0,-1], 'strength':9}
 force=[force_unit for x in range(ALL_FORCE_SENSOR)]
