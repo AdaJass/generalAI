9884b98edcef0b83b2d4a2d71bdb1ad66e2f1952
adajass <jass.ada@qq.com>
2018-05-06 04:16:15 +0800

    dkjg

---
+++ IO/outpt.py
@@ -0,0 +1,38 @@
+"""require module PyUserInput from https://github.com/PyUserInput/PyUserInput
+ 
+ this module provide output api help to control character.
+"""
+
+
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+
+MS = PyMouse()
+KB = PyKeyboard()
+
+def moveForward():
+    pass
+
+def moveLeft():
+    pass
+
+def moveRight():
+    pass
+
+def moveBack():
+    pass
+
+def turnLeft():
+    pass
+
+def turnRight():
+    pass
+
+def trunOver():
+    pass
+
+def turnDegree(angle):
+    pass
+
+
+
